fe1322f5af62b8156befb35491b3a414875256be
chechaluque <cesarluque2000@gmail.com>
2017-02-22 12:31:38 -0800

    Fix price format

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,8 @@
 class PagesController < ApplicationController
 
   def index
-    @products = Product.all
+    @products = Product.order('created_at DESC')
+    @productsorder = Product.limit(6)
   end
 
   def shop
